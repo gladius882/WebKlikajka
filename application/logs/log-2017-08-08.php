ff2578d49aa419401184746db8d2e8006ad4e11e
= <oldmanstudio.kontakt@gmail.com>
2017-08-08 22:01:08 +0200

    README.md created

---
+++ application/logs/log-2017-08-08.php
@@ -0,0 +1,36 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-08-08 18:17:39 --> Config Class Initialized
+INFO - 2017-08-08 18:17:39 --> Hooks Class Initialized
+DEBUG - 2017-08-08 18:17:40 --> UTF-8 Support Enabled
+INFO - 2017-08-08 18:17:40 --> Utf8 Class Initialized
+INFO - 2017-08-08 18:17:40 --> URI Class Initialized
+DEBUG - 2017-08-08 18:17:40 --> No URI present. Default controller set.
+INFO - 2017-08-08 18:17:40 --> Router Class Initialized
+INFO - 2017-08-08 18:17:40 --> Output Class Initialized
+INFO - 2017-08-08 18:17:40 --> Security Class Initialized
+DEBUG - 2017-08-08 18:17:40 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-08-08 18:17:40 --> Input Class Initialized
+INFO - 2017-08-08 18:17:40 --> Language Class Initialized
+INFO - 2017-08-08 18:17:40 --> Loader Class Initialized
+INFO - 2017-08-08 18:17:40 --> Controller Class Initialized
+INFO - 2017-08-08 18:17:40 --> File loaded: C:\xampp\htdocs\WebKlikajka\application\views\welcome_message.php
+INFO - 2017-08-08 18:17:40 --> Final output sent to browser
+DEBUG - 2017-08-08 18:17:40 --> Total execution time: 2.3233
+INFO - 2017-08-08 18:19:09 --> Config Class Initialized
+INFO - 2017-08-08 18:19:09 --> Hooks Class Initialized
+DEBUG - 2017-08-08 18:19:09 --> UTF-8 Support Enabled
+INFO - 2017-08-08 18:19:09 --> Utf8 Class Initialized
+INFO - 2017-08-08 18:19:09 --> URI Class Initialized
+DEBUG - 2017-08-08 18:19:09 --> No URI present. Default controller set.
+INFO - 2017-08-08 18:19:09 --> Router Class Initialized
+INFO - 2017-08-08 18:19:09 --> Output Class Initialized
+INFO - 2017-08-08 18:19:09 --> Security Class Initialized
+DEBUG - 2017-08-08 18:19:09 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-08-08 18:19:09 --> Input Class Initialized
+INFO - 2017-08-08 18:19:09 --> Language Class Initialized
+INFO - 2017-08-08 18:19:09 --> Loader Class Initialized
+INFO - 2017-08-08 18:19:09 --> Controller Class Initialized
+INFO - 2017-08-08 18:19:09 --> File loaded: C:\xampp\htdocs\WebKlikajka\application\views\welcome_message.php
+INFO - 2017-08-08 18:19:09 --> Final output sent to browser
+DEBUG - 2017-08-08 18:19:09 --> Total execution time: 0.0401
